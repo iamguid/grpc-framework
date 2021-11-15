45676cbedaca210debcaf97edfd983527c659a48
Vladislav Lebedev <iam.guid@gmail.com>
2021-11-16 01:45:29 +0500

    Small refactoring

--- parser/src/utils.ts
+++ parser/src/utils.ts
@@ -1,152 +1,95 @@
-export function filePathToPseudoNamespace(filePath: string): string {
-    return filePath
-      .replace(/\//g, '_')
-      .replace(/\./g, '_')
-      .replace(/\-/g, '_');
-  }
-  
-  export function buildNamespace(...items: (string | undefined | null)[]) {
-    let result = '';
-  
-    for (const item of items) {
-      if (!item) {
-        continue;
-      }
+import { ConstantContext, OptionStatementContext, EnumValueOptionContext, FieldOptionContext } from "./generated/Protobuf3Parser";
+import { Options } from "./reflection/Options";
 
-      const trimmed = trimChar(item, '.');
-      const isEmpty = trimmed === null || trimmed === '' || trimmed === undefined;
-  
-      if (!isEmpty) {
-        result += '.' + item;
-      }
+export function buildNamespace(...items: (string | undefined | null)[]) {
+  let result = '';
+
+  for (const item of items) {
+    if (!item) {
+      continue;
     }
-  
-    return result.substring(1);
-  }
-  
-  export function trimChar(str: string, charToRemove: string) {
-    while (str.charAt(0) === charToRemove) {
-      str = str.substring(1);
+
+    const trimmed = trimChar(item, '.');
+    const isEmpty = trimmed === null || trimmed === '' || trimmed === undefined;
+
+    if (!isEmpty) {
+      result += '.' + item;
     }
-  
-    while (str.charAt(str.length - 1) === charToRemove) {
-      str = str.substring(0, str.length - 1);
+  }
+
+  return result.substring(1);
+}
+
+export const extractOptionConstantValue = (ctx: ConstantContext): any => {
+    let result: any = null;
+
+    if (ctx.boolLit()) {
+        result = ctx.boolLit()!.text === "true";
     }
-  
-    return str;
-  }
-  
-  export function upperCaseFirst(str: string): string {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  
-  export function lowerCaseFirst(str: string): string {
-    return str.charAt(0).toLowerCase() + str.slice(1);
-  }
-  
-  export function snakeToCamel(str: string): string {
-    return str.replace(/(\_\w)/g, function(m) {
-      return m[1].toUpperCase();
-    });
-  }
-  
-  export function getPathToRoot(fileName: string) {
-    const depth = fileName.split('/').length;
-    return depth === 1 ? './' : new Array(depth).join('../');
-  }
-  
-  export function replaceProtoSuffix(protoFilePath: string, newSuffix: string): string {
-    const suffix = '.proto';
-    const hasProtoSuffix = protoFilePath.slice(protoFilePath.length - suffix.length) === suffix;
-    return hasProtoSuffix
-      ? protoFilePath.slice(0, -suffix.length) + '_' + newSuffix
-      : protoFilePath;
-  }
-  
-  export function withAllStdIn(callback: (buffer: Buffer) => void): void {
-    const ret: Buffer[] = [];
-    let len = 0;
-  
-    const stdin = process.stdin;
-    stdin.on('readable', function () {
-      let chunk;
-  
-      while ((chunk = stdin.read())) {
-        if (!(chunk instanceof Buffer)) throw new Error('Did not receive buffer');
-        ret.push(chunk);
-        len += chunk.length;
-      }
-    });
-  
-    stdin.on('end', function () {
-      callback(Buffer.concat(ret, len));
-    });
-  }
-  
-  // normaliseFieldObjectName modifies the field name that appears in the `asObject` representation
-  // to match the logic found in `protobuf/compiler/js/js_generator.cc`. See: https://goo.gl/tX1dPQ
-  export function normaliseFieldObjectName(name: string): string {
-    switch (name) {
-      case 'abstract':
-      case 'boolean':
-      case 'break':
-      case 'byte':
-      case 'case':
-      case 'catch':
-      case 'char':
-      case 'class':
-      case 'const':
-      case 'continue':
-      case 'debugger':
-      case 'default':
-      case 'delete':
-      case 'do':
-      case 'double':
-      case 'else':
-      case 'enum':
-      case 'export':
-      case 'extends':
-      case 'false':
-      case 'final':
-      case 'finally':
-      case 'float':
-      case 'for':
-      case 'function':
-      case 'goto':
-      case 'if':
-      case 'implements':
-      case 'import':
-      case 'in':
-      case 'instanceof':
-      case 'int':
-      case 'interface':
-      case 'long':
-      case 'native':
-      case 'new':
-      case 'null':
-      case 'package':
-      case 'private':
-      case 'protected':
-      case 'public':
-      case 'return':
-      case 'short':
-      case 'static':
-      case 'super':
-      case 'switch':
-      case 'synchronized':
-      case 'this':
-      case 'throw':
-      case 'throws':
-      case 'transient':
-      case 'try':
-      case 'typeof':
-      case 'var':
-      case 'void':
-      case 'volatile':
-      case 'while':
-      case 'with':
-        return `pb_${name}`;
+
+    if (ctx.strLit()) {
+        result = trimChar(ctx.strLit()!.text, "\"");
     }
-    return name;
+
+    if (ctx.intLit()) {
+        if (ctx.MINUS()) {
+            result = -Number.parseInt(ctx.intLit()!.text);
+        } else {
+            result = Number.parseInt(ctx.intLit()!.text);
+        }
+    }
+
+    if (ctx.floatLit()) {
+        if (ctx.MINUS()) {
+            result = -Number.parseFloat(ctx.floatLit()!.text);
+        } else {
+            result = Number.parseFloat(ctx.floatLit()!.text);
+        }
+    }
+
+    if (ctx.fullIdent()) {
+        result = ctx.fullIdent()!.text;
+    }
+
+    if (ctx.blockLit()) {
+        result = {}
+
+        const idents = ctx.blockLit()!.ident();
+        const constants = ctx.blockLit()!.constant();
+
+        idents.forEach((ident, index) => {
+            result[ident.text] = extractOptionConstantValue(constants[index]);
+        })
+    }
+
+    return result
+}
+
+export const extractOptions = (ctx: OptionStatementContext | EnumValueOptionContext | FieldOptionContext): Options => {
+    const options: Options = {};
+
+    const fullIndent = ctx.optionName().fullIdent();
+    let optionName = fullIndent[0].text;
+        
+    if (fullIndent.length === 2) {
+        optionName += "." + fullIndent[1].text;
+    }
+
+    let optionValue: any = extractOptionConstantValue(ctx.constant());
+
+    options[optionName] = optionValue;
+
+    return options;
+}
+
+export const trimChar = (str: string, charToRemove: string) => {
+  while (str.charAt(0) === charToRemove) {
+    str = str.substring(1);
   }
-  
+
+  while (str.charAt(str.length - 1) === charToRemove) {
+    str = str.substring(0, str.length - 1);
+  }
+
+  return str;
+}
---
+++ parser/src/visitor.ts
@@ -0,0 +1,283 @@
+import { AbstractParseTreeVisitor } from "antlr4ts/tree/AbstractParseTreeVisitor";
+import { ProtoContext, ImportStatementContext, Protobuf3Parser, ServiceDefContext, RpcContext, MessageDefContext, MessageElementContext, FieldContext, MapFieldContext, OneofFieldContext, EnumDefContext, EnumFieldContext } from "./generated/Protobuf3Parser";
+import { Protobuf3Visitor } from "./generated/Protobuf3Visitor";
+import { EnumDescriptor } from "./reflection/EnumDescriptor";
+import { EnumFieldDescriptor } from "./reflection/EnumFieldDescriptor";
+import { FieldDescriptor, MapField } from "./reflection/FieldDescriptor";
+import { FileDescriptor, FileImport } from "./reflection/FileDescriptor";
+import { IDescriptor } from "./reflection/IDescriptor";
+import { MessageDescriptor } from "./reflection/MessageDescriptor";
+import { MethodDescriptor } from "./reflection/MethodDescriptor";
+import { ServiceDescriptor } from "./reflection/ServiceDescriptor";
+import { extractOptions, trimChar } from "./utils";
+
+export class Visitor extends AbstractParseTreeVisitor<IDescriptor> implements Protobuf3Visitor<IDescriptor> {
+    public fileDescriptor: FileDescriptor;
+
+    private namespace: string[] = [];
+
+    constructor() {
+        super();
+        this.fileDescriptor = new FileDescriptor()
+    }
+
+    defaultResult() {
+        return this.fileDescriptor;
+    }
+
+    visitProto(ctx: ProtoContext): FileDescriptor {
+        const topLevelDefs = ctx.topLevelDef();
+
+        const packge = ctx.packageStatement()[0]?.fullIdent()?.text || "";
+        this.fileDescriptor.package = packge;
+        this.namespace.push(packge);
+
+        ctx.importStatement().forEach(importStatement => {
+            this.visitImportStatement(importStatement);
+        });
+
+        const options = ctx.optionStatement()
+            .map(optionStatement => extractOptions(optionStatement));
+
+        const services = topLevelDefs
+            .filter(topLevelDef => Boolean(topLevelDef.serviceDef()))
+            .map((topLevelDef, index) => this.visitServiceDef(topLevelDef.serviceDef()!, index));
+
+        const messages = topLevelDefs
+            .filter(topLevelDef => Boolean(topLevelDef.messageDef()))
+            .map((topLevelDef, index) => this.visitMessageDef(topLevelDef.messageDef()!, index))
+
+        const enums = topLevelDefs
+            .filter(topLevelDef => Boolean(topLevelDef.enumDef()))
+            .map((topLevelDef, index) => this.visitEnumDef(topLevelDef.enumDef()!, index))
+
+        this.fileDescriptor.options.push(...options);
+        this.fileDescriptor.services.push(...services);
+        this.fileDescriptor.messages.push(...messages);
+        this.fileDescriptor.enums.push(...enums);
+
+        return this.fileDescriptor;
+    }
+
+    visitImportStatement(ctx: ImportStatementContext): FileDescriptor {
+        const importType = ctx.tryGetToken(Protobuf3Parser.WEAK, 0)?.text;
+        const importPath = trimChar(ctx.strLit().text, "\"");
+
+        let imprt: FileImport = {
+            type: 'public',
+            path: importPath,
+        }
+
+        switch (importType) {
+            case 'weak': imprt.type = 'weak'; break;
+            case 'public': imprt.type = 'public'; break;
+        }
+
+        this.fileDescriptor.imports.push(imprt);
+
+        return this.fileDescriptor;
+    }
+
+    visitServiceDef(ctx: ServiceDefContext, index = 0): ServiceDescriptor {
+        const serviceName = ctx.serviceName().text
+
+        this.namespace.push(serviceName);
+        const methods = ctx.serviceElement()
+            .filter(serviceElement => Boolean(serviceElement.rpc()))
+            .map((serviceElement, index) => this.visitRpc(serviceElement.rpc()!, index))
+        this.namespace.pop();
+
+        const options = ctx.serviceElement()
+            .filter(serviceElement => Boolean(serviceElement.optionStatement()))
+            .map(serviceElement => extractOptions(serviceElement.optionStatement()!));
+
+        return new ServiceDescriptor({
+            index,
+            name: serviceName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options,
+            methods
+        });
+    }
+
+    visitRpc(ctx: RpcContext, index = 0): MethodDescriptor {
+        const options = ctx.optionStatement()
+            .map(optionStatement => extractOptions(optionStatement));
+
+        return new MethodDescriptor({
+            index,
+            name: ctx.rpcName().text,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options,
+            isClientStreaming: Boolean(ctx.tryGetToken(Protobuf3Parser.STREAM, 0)),
+            isServerStreaming: Boolean(ctx.tryGetToken(Protobuf3Parser.STREAM, 1)),
+            inputMessageType: ctx.messageType(0).text,
+            outputMessageType: ctx.messageType(1).text,
+        });
+    }
+
+    visitMessageDef(ctx: MessageDefContext, index = 0): MessageDescriptor {
+        const messageName = ctx.messageName().text;
+        this.namespace.push(messageName);
+
+        const nestedMessages = ctx.messageBody().messageElement()
+            .filter(messageElement => Boolean(messageElement.messageDef()))
+            .map((messageElement, index) => this.visitMessageDef(messageElement.messageDef()!, index));
+
+        const nestedEnums = ctx.messageBody().messageElement()
+            .filter(messageElement => Boolean(messageElement.enumDef()))
+            .map((messageElement, index) => this.visitEnumDef(messageElement.enumDef()!, index));
+
+        const fields = ctx.messageBody().messageElement()
+            .filter(messageElement => {
+                return Boolean(messageElement.field()) 
+                    || Boolean(messageElement.oneof()) 
+                    || Boolean(messageElement.mapField())
+            })
+            .reduce((current: FieldDescriptor[], messageElement: MessageElementContext, index) => {
+                if (Boolean(messageElement.field())) {
+                    current.push(this.visitField(messageElement.field()!, index));
+                }
+
+                if (Boolean(messageElement.mapField())) {
+                    current.push(this.visitMapField(messageElement.mapField()!, index));
+                }
+
+                if (Boolean(messageElement.oneof())) {
+                    const oneof = messageElement.oneof()!;
+                    const oneofName = oneof.oneofName().text;
+                    const fields = oneof.oneofField();
+
+                    fields.forEach((field, oneofIndex) => {
+                        current.push(this.visitOneofField(field, index + oneofIndex, oneofName)) 
+                    });
+                }
+
+                return current;
+            }, [])
+
+        this.namespace.pop();
+
+        const options = ctx.messageBody().messageElement()
+            .filter(messageElement => Boolean(messageElement.optionStatement()))
+            .map(messageElement => extractOptions(messageElement.optionStatement()!))
+
+        return new MessageDescriptor({
+            index,
+            name: messageName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options,
+            fields,
+            messages: nestedMessages,
+            enums: nestedEnums
+        });
+    }
+
+    visitField(ctx: FieldContext, index = 0): FieldDescriptor {
+        const fieldName = ctx.fieldName().text;
+        const filedType = ctx.type_().text;
+        const fieldNumber = Number.parseInt(ctx.fieldNumber().text, 10);
+        const isRepeated = Boolean(ctx.REPEATED()?.text);
+
+        const options = ctx.fieldOptions()?.fieldOption()
+            .map(fieldOption => extractOptions(fieldOption));
+
+        return new FieldDescriptor({
+            index,
+            name: fieldName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options: options || [],
+            type: filedType,
+            repeated: isRepeated,
+            fieldNumber 
+        });
+    }
+
+    visitMapField(ctx: MapFieldContext, index = 0): FieldDescriptor {
+        const fieldName = ctx.mapName().text;
+        const fieldNumber = Number.parseInt(ctx.fieldNumber().text, 10);
+        const keyType = ctx.keyType().text;
+        const valueType = ctx.type_().text;
+        const mapField: MapField = { keyType, valueType }
+
+        const options = ctx.fieldOptions()?.fieldOption()
+            .map(fieldOption => extractOptions(fieldOption));
+
+        return new FieldDescriptor({
+            index,
+            name: fieldName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options: options || [],
+            type: "",
+            repeated: false,
+            fieldNumber,
+            map: mapField
+        });
+    }
+
+    visitOneofField(ctx: OneofFieldContext, index = 0, oneofName = ''): FieldDescriptor {
+        const fieldName = ctx.fieldName().text;
+        const filedType = ctx.type_().text;
+        const fieldNumber = Number.parseInt(ctx.fieldNumber().text, 10);
+
+        const options = ctx.fieldOptions()?.fieldOption()
+            .map(fieldOption => extractOptions(fieldOption));
+
+        return new FieldDescriptor({
+            index,
+            name: fieldName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options: options || [],
+            type: filedType,
+            repeated: false,
+            fieldNumber 
+        });
+    }
+
+    visitEnumDef(ctx: EnumDefContext, index = 0): EnumDescriptor {
+        const enumName = ctx.enumName().text;
+
+        this.namespace.push(enumName);
+        const fields = ctx.enumBody().enumElement()
+            .filter(enumElement => Boolean(enumElement.enumField()))
+            .map((enumElement, index) => this.visitEnumField(enumElement.enumField()!, index))
+        this.namespace.pop();
+
+        const options = ctx.enumBody().enumElement()
+            .filter(enumElement => Boolean(enumElement.optionStatement()))
+            .map(enumElement => extractOptions(enumElement.optionStatement()!))
+
+        return new EnumDescriptor({
+            index,
+            name: enumName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options,
+            fields
+        });
+    }
+
+    visitEnumField(ctx: EnumFieldContext, index = 0): EnumFieldDescriptor {
+        const fieldName = ctx.ident().text;
+        const fieldValue = Number.parseInt(ctx.intLit().text);
+
+        const options = ctx.enumValueOptions()?.enumValueOption()
+            .map(enumValueOption => extractOptions(enumValueOption))
+
+        return new EnumFieldDescriptor({
+            index,
+            name: fieldName,
+            namespace: this.namespace.join('.'),
+            fileDescriptor: this.fileDescriptor,
+            options: options || [],
+            fieldName: fieldName,
+            fieldValue: fieldValue
+        });
+    }
+}
